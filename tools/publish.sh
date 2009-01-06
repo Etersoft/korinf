602eda836b4cbf556c06c32f2691bbcc55a574b8
Vitaly Lipatov <lav@etersoft.ru>
2009-01-06 22:36:52 +0300

    publish: use --all and --tags

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 PROJECT=korinf
+git push --all git.eter:packages/$PROJECT.git
+git push --all git.alt:packages/$PROJECT.git
 git push --tags git.eter:packages/$PROJECT.git
 git push --tags git.alt:packages/$PROJECT.git
