ce826958121d77bbc656ec0aa0e28b87f3d693ae
Vitaly Lipatov <lav@etersoft.ru>
2009-01-29 14:55:31 +0300

    add cifs build script

---
+++ bin-wine/wine-cifs.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# 2005, 2006, 2007, 2008 (c) Etersoft http://etersoft.ru
+# Author: Vitaly Lipatov <lav@etersoft.ru>
+# GNU Public License version 3
+#
+
+# load common functions, compatible with local and installed script
+. `dirname $0`/../share/eterbuild/korinf/common
+kormod korinf
+
+NAME=etercifs
+
+build_project $WINEPUB_PATH/../CIFS@Etersoft $NAME "" $@
