17c20240d874df48cd6121365850298332777a1f
Vitaly Lipatov <lav@etersoft.ru>
2009-02-17 19:32:15 +0300

    add script for build all private parts

---
+++ bin-wine/wine-etersoft-all.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# 2005, 2006, 2007, 2008 (c) Etersoft http://etersoft.ru
+# Author: Vitaly Lipatov <lav@etersoft.ru>
+# GNU Public License version 3
+
+#
+# load common functions, compatible with local and installed script
+. `dirname $0`/../share/eterbuild/korinf/common
+kormod korinf
+
+# Не отдавать с таким кодом
+ETERREGNUM=EEEE-C0DE
+# ETERREGNUM=EEEE-0003
+
+build_project $WINEETER_PATH wine-etersoft-local WINE-Local $@
+
+build_project $WINEETER_PATH wine-etersoft-network WINE-Network $@
+
+build_project $WINEETER_PATH wine-etersoft-networklite WINE-NetworkLite $@
+
+build_project $WINEETER_PATH wine-etersoft-networklite WINE-NetworkUnique $@
+
+build_project $WINEETER_PATH wine-etersoft-sql WINE-SQL $@
+
+build_project $WINEETER_PATH wine-etersoft-sqlunique WINE-SQLUnique $@
