d35bf7b2ee1fb874a8ef899ff098b896e9cd41c8
Yuri Fil <yurifil@etersoft.ru>
2009-02-18 18:36:01 +0300

    script for postgresql

---
+++ bin-common/build-postgres.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# 2005, 2006, 2007, 2008 (c) Etersoft http://etersoft.ru
+# Author: Vitaly Lipatov <lav@etersoft.ru>
+# GNU Public License version 3
+
+#
+# load common functions, compatible with local and installed script
+. `dirname $0`/../share/eterbuild/korinf/common
+kormod korinf
+
+major=8.3
+minor=6
+pname=postgresql
+pnamever=${pname}-${major}eter
+POSTGRESDIR=/var/ftp/pub/Etersoft/PostgreSQL/
+
+export MAINFILES="${pnamever}[-_][0-9] ${pnamever}-contrib[-_][0-9] ${pnamever}-libs[-_][0-9] ${pnamever}-server[-_][0-9]"
+export EXTRAFILES="${pnamever}-devel[-_][0-9] ${pnamever}-docs[-_][0-9] ${pnamever}-plperl[-_][0-9] ${pnamever}-plpython[-_][0-9] ${pnamever}-test[-_][0-9]"
+
+build_project $POSTGRESDIR ${pnamever} "" $major.$minor $@
