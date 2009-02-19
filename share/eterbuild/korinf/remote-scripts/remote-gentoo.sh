3c0bde118428b5b6241a67eae451c10b226695a2
Yuri Fil <yurifil@etersoft.ru>
2009-02-19 19:51:42 +0300

    --buildpkg changed to --buildpkgonly

--- share/eterbuild/korinf/remote-scripts/remote-gentoo.sh
+++ share/eterbuild/korinf/remote-scripts/remote-gentoo.sh
@@ -36,9 +36,9 @@ sed -i "1iWINENUMVERSION=$WINENUMVERSION" $PACKAGE/*.ebuild || exit 1
 # --buildpkgonly 
 export WINENUMVERSION
 if [ -e /usr/bin/sudo ] ; then 
-    FEATURES="nostrip noclean" sudo emerge --verbose --digest --buildpkg $PACKAGE || exit 1
+    FEATURES="nostrip noclean" sudo emerge --verbose --digest --buildpkgonly $PACKAGE || exit 1
 else 
-    FEATURES="nostrip noclean" emerge --verbose --digest --buildpkg $PACKAGE || exit 1
+    FEATURES="nostrip noclean" emerge --verbose --digest --buildpkgonly $PACKAGE || exit 1
 fi
 
 exit 0
