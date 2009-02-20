8b8e049f14e126ce82127318fdff3fb5852d465b
Vitaly Lipatov <lav@etersoft.ru>
2009-02-20 13:39:17 +0300

    relicense all files to GNU AGPLv3

--- robot/arobot-wine.sh
+++ robot/arobot-wine.sh
@@ -7,17 +7,17 @@
 #  Copyright (c) Etersoft <http://etersoft.ru> 2006, 2007, 2009
 #  Copyright (c) Vitaly Lipatov <lav@etersoft.ru> 2006, 2007, 2009
 #
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 3 of the License, or
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
-#
+
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
+#  GNU Affero General Public License for more details.
+
+#  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##
 
