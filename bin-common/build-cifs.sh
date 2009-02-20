c64a9fb3b9dd955639efb280752c11c945043e3f
Vitaly Lipatov <lav@etersoft.ru>
2009-02-20 13:32:44 +0300

    move build-cifs script

--- bin-common/build-cifs.sh
+++ bin-common/build-cifs.sh
@@ -2,14 +2,11 @@
 # 2005, 2006, 2007, 2008 (c) Etersoft http://etersoft.ru
 # Author: Vitaly Lipatov <lav@etersoft.ru>
 # GNU Public License version 3
-
 #
+
 # load common functions, compatible with local and installed script
 . `dirname $0`/../share/eterbuild/korinf/common
 kormod korinf
 
-CIFSDIR=/var/ftp/pub/Etersoft/CIFS@Etersoft
-CIFSVERSION=4.1.1
-
-#build_project $CIFSDIR etercifs "" $CIFSVERSION $@
-build_project $CIFSDIR dkms-etercifs "" $CIFSVERSION $@
+build_project $WINEPUB_PATH/../CIFS@Etersoft etercifs "" $@
+#build_project $WINEPUB_PATH/../CIFS@Etersoft dkms-etercifs "" $@
