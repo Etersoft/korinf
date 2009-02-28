8c4160485424d36ff77a68f6fe2cfe3e7c84217b
Vitaly Lipatov <lav@etersoft.ru>
2009-02-28 19:48:36 +0300

    improve get_distro_list to support . file for include sub lists from the current dir

---
+++ tests/test_distro_list.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# 2006 (c) Etersoft www.etersoft.ru
+# Public domain
+
+# load common functions, compatible with local and installed script
+. `dirname $0`/../share/eterbuild/korinf/common
+
+check()
+{
+	local REPL
+	TEST1="$1"
+	REPL1="$2"
+	REAL=`get_distro_list "$TEST1"`
+	echo
+	echo -n "Source line: '$TEST1' with result '$REAL' (wait for '$REPL1') "
+	#REPL=`echo $TEST1 | sed -r -e $NRL`
+	#REPL=`echo $TEST1 | perl -pi "$NRL"`
+	[ "$REPL1" != "$REAL" ] && failure || success
+	echo
+}
+
+check SomeDistro/2010 "SomeDistro/2010"
+check "SomeDistro/2010 SomeLinux/2020" "SomeDistro/2010 SomeLinux/2020"
+
+cat <<EOF >test1
+#
+SomeDistro/2010
+SomeLinux/2020
+EOF
+check test1 "SomeDistro/2010
+SomeLinux/2020"
+
+cat <<EOF >test2
+#
+. test1
+EndLinux/2038
+EOF
+check test2 "EndLinux/2038
+SomeDistro/2010
+SomeLinux/2020"
+
+rm -f test1 test2
