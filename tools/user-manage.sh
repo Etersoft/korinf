4b0e8149ab31f92385189e46bce270d851a0d14d
Yuri <yurifil@etersoft.ru>
2009-03-02 15:16:28 +0300

    - REBUILDLIST support - fix argument order

--- tools/user-manage.sh
+++ tools/user-manage.sh
@@ -1,33 +1,55 @@
 #!/bin/sh
 # 2006 (c) Etersoft www.etersoft.ru
 # Public domain
+#
+#Usage:
+#user_manage.sh {add, delete, change} NEWUSER NEWGROUP OLDUSER
+#
+
 
 # load common functions, compatible with local and installed script
-. ../share/eterbuild/korinf/common
-kormod mount korinf
+. `dirname $0`/../share/eterbuild/korinf/common
+kormod korinf
 
 TESTDIR=/tmp/autobuild/testlog
 mkdir -p $TESTDIR
 
 UCMD=$1
+
 CHUSER=$2
-
-if [ -z $2] ; then
-	CHGROUP=$2
+shift 2
+if [ -z $1] ; then
+	CHGROUP=$CHUSER
+	shift
 else
-	CHGROUP=$3
+	CHGROUP=$1
+	shift
 fi
+OLDUSER=$1
 
-for i in $(get_distro_list $KORINFETC/lists/all) ; do
+set_rebuild_list
+CMDRE=$(get_distro_list $REBUILDLIST)
+[ -z "$CMDRE" ] && fatal "build list is empty"
+
+
+for i in $CMDRE ; do
 	echo Mount $i ...
-        $SUDO mount $LINUXHOST/$i $TESTDIR --bind || exit 1
+#        $SUDO mount $LINUXHOST/$i $TESTDIR --bind || exit 1
 
-	mkdir -p $TESTDIR/tmp
-        cp -f  ./remote-$UCMD-user.sh $TESTDIR/tmp || { warning "Cannot copy script" ; return 1 ; }
+#	mkdir -p $TESTDIR/tmp
+#        cp -f  ./remote-$UCMD-user.sh $TESTDIR/tmp || { warning "Cannot copy script" ; return 1 ; }
 	echo Chrooting and executing...
-        $SUDO chroot $TESTDIR su - -c "sh -x /tmp/remote-$UCMD-user.sh \"$CHUSER\" \"$CHGROUP\""
+#        $SUDO chroot $TESTDIR su - -c "sh -x /tmp/remote-$UCMD-user.sh \"$CHUSER\" \"$CHGROUP\" \"$OLDUSER\""
+
 #test
-#	$SUDO chroot $TESTDIR su - -c "mkdir -p /tmp/add-user"
+#	$SUDO chroot $TESTDIR su - -c "mkdir -p /tmp/add-user-$CHUSER"
+	echo "command $UCMD"
+	echo "new user $CHUSER"
+	echo "new group $CHGROUP"
+	echo "old user $OLDUSER"
+	echo "system $i"
+#end of test
+
 	$SUDO umount $TESTDIR && echo "Unmount $i"
 done
 
