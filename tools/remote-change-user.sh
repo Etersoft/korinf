64062e9f1ccf5e372ec3a402e46c082f0f6bce33
Yuri <yurifil@etersoft.ru>
2009-03-02 16:07:22 +0300

    homedir added

--- tools/remote-change-user.sh
+++ tools/remote-change-user.sh
@@ -7,15 +7,27 @@
 NEWUSER=$1
 NEWGROUP=$2
 OLDUSER=$3
+OLDGROUP=$4
 ATIME=`date +%T`
 
+mkdir -p /home/$NEWUSER
 #user:
 cp /etc/passwd /etc/passwd.old.$ATIME
 sed "s|$OLDUSER|$NEWUSER|g" /etc/passwd > /etc/passwd.tmp
 mv -f /etc/passwd.tmp /etc/passwd
 rm -f /etc/passwd.tmp
+
+cp /etc/shadow /etc/shadow.old.$ATIME
+sed "s|$OLDUSER|$NEWUSER|g" /etc/shadow > /etc/shadow.tmp
+mv -f /etc/shadow.tmp /etc/shadow
+rm -f /etc/shadow.tmp
 #group:
 cp /etc/group /etc/group.old.$ATIME
-sed "s|$OLDUSER|$NEWUSER|g" /etc/group > /etc/group.tmp
+sed "s|$OLDGROUP|$NEWGROUP|g" /etc/group > /etc/group.tmp
 mv -f /etc/group.tmp /etc/group
 rm -f /etc/group.tmp
+
+cp /etc/gshadow /etc/gshadow.old.$ATIME
+sed "s|$OLDGROUP|$NEWGROUP|g" /etc/gshadow > /etc/gshadow.tmp
+mv -f /etc/gshadow.tmp /etc/gshadow
+rm -f /etc/gshadow.tmp
