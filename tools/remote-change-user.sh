a2c2a5db96de2946ac9a5c47776d05548f6059a9
Yuri <yurifil@etersoft.ru>
2009-03-02 15:26:44 +0300

    - fix date - fix sed arguments

--- tools/remote-change-user.sh
+++ tools/remote-change-user.sh
@@ -3,20 +3,19 @@
 # Public domain
 
 # load common functions, compatible with local and installed script
-. ../share/eterbuild/korinf/common
-kormod mount
 
-OLDUSER=$1
-NEWUSER=$2
-ATIME=`date +%X`
+NEWUSER=$1
+NEWGROUP=$2
+OLDUSER=$3
+ATIME=`date +%T`
 
 #user:
 cp /etc/passwd /etc/passwd.old.$ATIME
-sed 's|$OLDUSER|$NEWUSER|g' /etc/passwd > /etc/passwd.tmp
+sed "s|$OLDUSER|$NEWUSER|g" /etc/passwd > /etc/passwd.tmp
 mv -f /etc/passwd.tmp /etc/passwd
 rm -f /etc/passwd.tmp
 #group:
 cp /etc/group /etc/group.old.$ATIME
-sed 's|$OLDUSER|$NEWUSER|g' /etc/group > /etc/group.tmp
+sed "s|$OLDUSER|$NEWUSER|g" /etc/group > /etc/group.tmp
 mv -f /etc/group.tmp /etc/group
 rm -f /etc/group.tmp
