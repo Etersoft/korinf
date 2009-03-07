3fd2f4ed29aec0fc875ca3573463b8ba9351305b
Vitaly Lipatov <lav@etersoft.ru>
2009-03-07 16:42:46 +0300

    improve release_check.sh for build support

--- bin-wine/release-check.sh
+++ bin-wine/release-check.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
+
+# Script for check WINE@Etersoft release integrity
+# Params:
+# ./release-check.sh -b DISTRO  build all packages for the DISTRO
+# ./release-check.sh DISTRO     print checks only for the DISTRO
+# ./release-check.sh            print overall check info for all distros
+
 RELEASEVERSION=last
+
+if [ "$1" = "-b" ] && [ -n "$2" ] ; then
+	CHECKONLY=
+	shift
+else
+	CHECKONLY="-c"
+fi
 
 if [ -n "$1" ] ; then
 	DISTR=$1
@@ -9,8 +23,8 @@ else
 	OPT=""
 fi
 
-./wine-etersoft.sh -c $DISTR $RELEASEVERSION
-./wine-etersoft-all.sh $OPT -c $DISTR $RELEASEVERSION
-./haspd.sh $OPT -c $DISTR $RELEASEVERSION
-./fonts-ttf-ms.sh $OPT -c $DISTR $RELEASEVERSION
-./fonts-ttf-liberation.sh $OPT -c $DISTR $RELEASEVERSION
+./wine-etersoft.sh $CHECKONLY $DISTR $RELEASEVERSION
+./wine-etersoft-all.sh $OPT $CHECKONLY $DISTR $RELEASEVERSION
+./haspd.sh $OPT $CHECKONLY $DISTR $RELEASEVERSION
+./fonts-ttf-ms.sh $OPT $CHECKONLY $DISTR $RELEASEVERSION
+./fonts-ttf-liberation.sh $OPT $CHECKONLY $DISTR $RELEASEVERSION
