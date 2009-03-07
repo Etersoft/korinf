1d58826ef1652644a0a55ae041c970939ac2c221
Vitaly Lipatov <lav@etersoft.ru>
2009-03-07 11:32:35 +0300

    move warning/fatal to message file to fix funcs intersection

--- robot/arobot.sh
+++ robot/arobot.sh
@@ -32,18 +32,20 @@ AROBOTDIR=`dirname $0`
 # load common functions, compatible with local and installed script
 . `dirname $0`/../share/eterbuild/korinf/common
 
-kormod korinf
+#kormod korinf
 set_log_dir
 # /`date "+%Y%m%d"`
 
 umask 0002
 
 . $AROBOTDIR/funcs/common
+. $AROBOTDIR/funcs/message
 . $AROBOTDIR/funcs/license
 . $AROBOTDIR/funcs/task
 . $AROBOTDIR/funcs/etersoft
 . $AROBOTDIR/funcs/mail
 . $AROBOTDIR/funcs/build
+
 . $AROBOTDIR/products/wine-compat
 . $AROBOTDIR/products/wine-etersoft
 . $AROBOTDIR/products/selta
