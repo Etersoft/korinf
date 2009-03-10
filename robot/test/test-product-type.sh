fcdf2c29aa082c94d8bf17cc44796c02e285f376
Etersoft builder <lav@etersoft.ru>
2009-03-11 00:33:13 +0300

    add test product type

---
+++ robot/test/test-product-type.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+cd ..
+# load common functions, compatible with local and installed script
+. `dirname $0`/../share/eterbuild/korinf/common
+
+AROBOTDIR=$(pwd)
+
+. $AROBOTDIR/funcs/common
+. $AROBOTDIR/funcs/task
+. $AROBOTDIR/funcs/etersoft
+
+check_o()
+{
+	echo -n "Source line: '$2' with result '$3' "
+	[ "$2" != "$3" ] && failure || success
+	echo
+}
+
+check()
+{
+	check_o "$1" "$2" "$(get_product_type "$1")"
+}
+
+check "Network Lite" "NetworkLite"
+check "SQL" "SQL"
