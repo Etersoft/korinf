a23fa621c50e9c42b097009caf60618c340ce2d1
Yuri Fil <yurifil@etersoft.ru>
2009-03-20 16:53:39 +0300

    fix for 1.0.10

--- tools/makeiso/functions.sh
+++ tools/makeiso/functions.sh
@@ -33,15 +33,6 @@ exit_handler()
 
 # Собирает и выкладывает на наш обычный FTP
 # Надо исправить, чтобы выкладывала во временную копию
-build_system()
-{
-	SYS=$1
-	echo "Build $SYS on builder"
-	BUSER=builder
-	ssh $BUSER@builder "export ETERREGNUM=$ETERREGNUM &&\
-		/home/$BUSER/Projects/korinf/makeiso/build-wine-package.sh $CPRODUCT "" $SYS $WINENUMVERSION/network"
-	[ $? != 0 ] && echo "FAIL with $SYS" >>$FILERES
-}
 
 makeiso()
 {
@@ -57,7 +48,7 @@ makeiso()
 	-m "*Special*" \
 	-publisher "Etersoft, wine@etersoft.ru" \
 	-sysid LINUX -o $PATHTOFTP/$FILENAME.building  -r -J -graft-points -quiet \
-	-f $LOCALWINEPUB_PATH$ALPHA $WINEETER_PATH$ALPHAP || exit 1
+	-f $WINEPUB_PATH$ALPHA $WINEETER_PATH$ALPHAP || exit 1
 	mv -f $PATHTOFTP/$FILENAME.building $PATHTOFTP/$FILENAME
 #	-m "*_Network*" -m "network_*" \
 #	-m "*_Local*" -m "local_*" \
@@ -70,33 +61,16 @@ build_packages()
 
 #trap exit_handler EXIT HUP INT QUIT PIPE TERM
 for i in `cat makeiso/$0.nums` ; do
-	ETERREGNUM=$i
 	echo
 	echo
-	echo "Build with $ETERREGNUM number"
-	FILENAME=wine$PRODUCTBASE-$ETERREGNUM.iso
+	echo "Build $PRODUCTBASE iso"
+	FILENAME=wine$PRODUCTBASE.iso
 	if [ -f $PATHTOFTP/$FILENAME ] ; then
 		echo "Already exists: $FILENAME. Skipping..."
 		continue
 	fi
 	# echo "Running all builds ..."
 	>$FILERES
-	if [ -z "$TESTBUILDDIST" ] ; then
-#		build_system bsd &
-		build_system alt &
-		build_system gen &
-#		echo
-	else
-		build_system $TESTBUILDDIST
-#		build_system alt
-#		build_system gen
-	fi
-	echo "Wait for finishing..."
-	wait
-	if grep FAIL $FILERES ; then
-		#cat $FILERES
-		exit 1
-	fi
 
 	if [ -n "$TESTBUILDDIST" ] ; then
 		echo "Test mode: Skip ISO image build"
@@ -104,9 +78,7 @@ for i in `cat makeiso/$0.nums` ; do
 	fi
 
 	ALPHA=/$WINENUMVERSION
-	ALPHAP=-disk/$WINENUMVERSION/$PRODUCTBASE
-	sed -e "s/XXXX-XXXX/$ETERREGNUM/g" <$WINEETER_PATH$ALPHAP/docs/README_$PRODUCT.html >$WINEETER_PATH$ALPHAP/README.html || exit 1
-	sed -e "s/XXXX-XXXX/$ETERREGNUM/g" <$WINEETER_PATH$ALPHAP/docs/license_$PRODUCTBASE.html >$WINEETER_PATH$ALPHAP/license.html || exit 1
+	ALPHAP=/$WINENUMVERSION/WINE-$PRODUCT
 
 	makeiso || exit 1
 	echo "Built $ETERREGNUM at `date`" >> $FILEREP
