698cc267ab1b89be9256c843d499f9654a8c0b9a
Vitaly Lipatov <lav@etersoft.ru>
2009-04-10 22:03:45 +0400

    buildstrap: fix build result checking

--- bin-common/build-strap.sh
+++ bin-common/build-strap.sh
@@ -26,13 +26,15 @@ BOOTSTRAP=1
 # Run with -s for real bootstrap
 if [ "$1" = "-s" ] ; then
 	WITHOUTEBU=1
+	NIGHTBUILD=1
 	shift
 fi
 
 if [ -n "$WITHOUTEBU" ] ; then
 	# Build without etersoft-build-utils using (just rpm -bb)
-	build_extrapkg rpm-build-altlinux-compat $@
-	build_extrapkg etersoft-build-utils $@
+	# FIXME: broken error code
+	build_extrapkg rpm-build-altlinux-compat $@ || exit 1
+	build_extrapkg etersoft-build-utils $@ || exit 1
 fi
 
 
