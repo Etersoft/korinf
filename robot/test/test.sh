1ccca93f622196925ef41011cd67fb5e5d543898
Vitaly Lipatov <lav@etersoft.ru>
2009-07-10 21:25:12 +0400

    update test for private part build

--- robot/test/test.sh
+++ robot/test/test.sh
@@ -3,8 +3,8 @@
 AROBOTDIR=$(pwd)/..
 
 #cp tasks/test-postgres.task ./try.task
-cp tasks/test-rx.task ./try.task
-#cp tasks/test-wine-etersoft.task ./try.task
+#cp tasks/test-rx.task ./try.task
+cp tasks/test-wine-etersoft.task ./try.task
 #cp tasks/test-wine.task ./try.task
 #cp tasks/test-selta.task ./try.task
 $AROBOTDIR/arobot.sh try.task
