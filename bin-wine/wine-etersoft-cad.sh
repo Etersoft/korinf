e40f3f1cabbc7f57fdea9f9e54089eed6859d946
Vitaly Lipatov <lav@etersoft.ru>
2009-07-27 22:04:06 +0400

    add new build script

---
+++ bin-wine/wine-etersoft-cad.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# 2005, 2006, 2007, 2008 (c) Etersoft http://etersoft.ru
+# Author: Vitaly Lipatov <lav@etersoft.ru>
+# GNU Public License version 3
+
+#
+# load common functions, compatible with local and installed script
+. `dirname $0`/../share/eterbuild/korinf/common
+kormod korinf
+
+build_project $WINEETER_PATH wine-etersoft-cad "WINE" $@
+
