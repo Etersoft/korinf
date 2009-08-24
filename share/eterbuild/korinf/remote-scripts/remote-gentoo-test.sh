760900e2572b1c749424c8144c957713de1291cb
Yuri Fil <yurifil@etersoft.ru>
2009-08-24 19:35:04 +0400

    some little fixes

--- share/eterbuild/korinf/remote-scripts/remote-gentoo-test.sh
+++ share/eterbuild/korinf/remote-scripts/remote-gentoo-test.sh
@@ -7,8 +7,8 @@
 # positional parameters
 COMMAND=$1
 PACKAGE="$2"
-RPMSDIR="$3"
-SOURCEURL="$4"
+#RPMSDIR="$3"
+SOURCEURL="$3"
 
 GROUP=app-emulation
 
@@ -57,13 +57,17 @@ gen_ebuild()
         # FIXME: how to get build package name?
         #PKGNAME=`querypackage "$SRPMNAME" NAME`
         # FIXME: problem with various version
-	WORKDIR=/usr/portage/${GROUP}/${PACKAGE}
-	RPMDIR=/home/$INTUSER/RPM/RPMS
+	LOCUSER=korinfer
+	WORKDIR=/home/$LOCUSER/tmp
+	RPMSDIR=/home/$LOCUSER/RPM/RPMS
 	EBUILDFILE=${WORKDIR}/${PACKAGE}.ebuild
-        BUILTRPM=$(ls -1 $RPMDIR/*.rpm | grep $PKGNAME | tail -n1)
+	export $EBUILDFILE
+        BUILTRPM=$(ls -1 $RPMSDIR/*.rpm | grep $PACKAGE | tail -n1)
+
 
 	mkdir -p $WORKDIR
 	cd $WORKDIR
+
 	echo "# Copyright 1999-2007 Gentoo Foundation" > $EBUILDFILE
 	echo "# Distributed under the terms of the GNU General Public License v2" >> $EBUILDFILE
 	echo '# $Header: $' >> $EBUILDFILE
@@ -72,26 +76,37 @@ gen_ebuild()
 	echo "DESCRIPTION=$DESCRIPTION" >> $EBUILDFILE
 	HOMEPAGE=`querypackage "$BUILTRPM" URL`
 	echo "HOMEPAGE=$HOMEPAGE" >> $EBUILDFILE
+
 	#FIXME: how to define SRC_URI
-	SRC_URI=""
+	SRC_URI="http://updates.etersoft.ru/pub/Etersoft/"
 	echo "SRC_URI=$SRC_URI" >> $EBUILDFILE
 	LICENSE=`querypackage "$BUILTRPM" LICENSE`
-	echo "LICENSE=$LICENSE`" >> $EBUILDFILE
+	echo "LICENSE=$LICENSE" >> $EBUILDFILE
 	echo 'SLOT="0"' >> $EBUILDFILE
 	echo 'KEYWORDS="-* x86 amd64"' >> $EBUILDFILE
+	echo "src_unpack() {" >> $EBUILDFILE
+	echo 'unpack ${A}' >> $EBUILDFILE
+	echo "}" >> $EBUILDFILE
+	echo >> $EBUILDFILE
 	echo "src_install() {" >> $EBUILDFILE
 	echo "cp -R "${WORKDIR}/${PACKAGE}" "${D}"" >> $EBUILDFILE
 	echo "}" >> $EBUILDFILE
 
+	echo
+	cat $EBUILDFILE
+	echo
+	cp $EBUILDFILE $RPMSDIR
+
         #get file hierarchy
-	cp $BUILTRPM ${WORKDIR}/${PACKAGE}
-        rpm2cpio $BUILTRPM | cpio -dimv || fatal "error with rpm2cpio"
-        rm -f $BUILTRPM
+#	cp $BUILTRPM ${WORKDIR}/${PACKAGE}
+#        rpm2cpio $BUILTRPM | cpio -dimv || fatal "error with rpm2cpio"
+#        rm -f $BUILTRPM
 
         # create package with the PACKAGE name (not src.rpm name)
-        rm -f ../$TARGETPKG
-        ebuild $EBUILDFILE package || fatal
-        cd -
+#        rm -f ../$TARGETPKG
+#        ebuild $EBUILDFILE package || fatal
+#        cd -
+
 }
 
 install_gentoo()
@@ -109,8 +124,8 @@ case $COMMAND in
         "build")
                 build_gentoo
                 ;;
-        "convert")
-                convert_gentoo && gen_ebuild
+        "generate")
+		gen_ebuild
                 ;;
         "install")
                 install_gentoo
