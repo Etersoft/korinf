c7b68dddb58142873a4bc31d01b852e4acfc31f5
Yuri Fil <yurifil@etersoft.ru>
2009-11-10 16:47:03 +0300

    new script to generate ebuilds and tarballs

--- share/eterbuild/korinf/remote-scripts/remote-gentoo.sh
+++ share/eterbuild/korinf/remote-scripts/remote-gentoo.sh
@@ -5,39 +5,146 @@
 
 # Internal build script for Gentoo
 # positional parameters
-PACKAGE="$1"
-ETERREGNUM="$2"
+COMMAND=$1
+PACKAGE="$2"
+#RPMSDIR="$3"
 SOURCEURL="$3"
+DESTURL="$5"
 
-echo "Start with PACKAGE: $PACKAGE ETERREGNUM: $ETERREGNUM, SOURCEURL - $SOURCEURL"
-mkdir -p /usr/portage/app-emulation
-cd /usr/portage/app-emulation || exit 1
+GROUP=app-emulation
 
-sudo chown -R $LOCUSER:portage /usr/portage/app-emulation/ || { echo "Cannot change owner" ; exit 1 ; }
+querypackage()
+{
+        rpmquery -p --queryformat "%{$2}" $1
+}
 
-# remove old files
-rm -rf $PACKAGE || exit 1
-rm -f /usr/portage/distfiles/$PACKAGE-*
-rm -f /usr/portage/packages/All/$PACKAGE*.tbz2
+fatal()
+{
+        echo $@ >&2
+        exit 1
+}
 
-# get ebuild file
-portfile="gentoo-$PACKAGE.tar.bz2"
-#porturl="ftp://server/$CAT/Etersoft/WINE@Etersoft-$WINENUMVERSION/sources/$portfile"
-porturl="$SOURCEURL/$portfile"
-rm -f $portfile && wget $porturl && tar xvfj $portfile || exit 1
 
-#sed -i 's|@ETERREGNUM@|${ETERREGNUM}|g' $PACKAGE/*.ebuild
-#export ETERREGNUM WINENUMVERSION
-# Hack:
-sed -i "1iETERREGNUM=$ETERREGNUM" $PACKAGE/*.ebuild || exit 1
-#sed -i "1iWINENUMVERSION=$WINENUMVERSION" $PACKAGE/*.ebuild || exit 1
-# do not export PACKAGE here
-# --buildpkgonly 
-#export WINENUMVERSION
-if [ -e /usr/bin/sudo ] ; then 
-    FEATURES="nostrip noclean" sudo emerge --verbose --digest --buildpkgonly $PACKAGE || exit 1
-else 
-    FEATURES="nostrip noclean" emerge --verbose --digest --buildpkgonly $PACKAGE || exit 1
-fi
+build_gentoo()
+{
+        RPMBUILDNODEPS="--nodeps"
+        RPMBUILDROOT="/home/$INTUSER/RPM/BUILD/$PACKAGE-$PKGVERSION"
+        # FIXME: x86_64 support
+        BUILDARCH=i586
+        rpmbuild -v --rebuild $RPMBUILDNODEPS --buildroot $RPMBUILDROOT $SRPMNAME --target $BUILDARCH
+}
+
+#convert to tar.gz
+convert_gentoo()
+{
+LOCUSER=korinfer
+BUILDERHOME=/home/$LOCUSER
+#PORTAGEDIR=/usr/local/portage
+#TEMPDIR=$BUILDERHOME/tmp/$PACKAGE
+#RPMDIR=$BUILDERHOME/RPM/BP
+
+echo
+echo "Changing to RPMSDIR=$RPMSDIR"
+pushd $RPMSDIR
+echo "Converting rpm to tar.gz"
+rpm2targz *$BUILDNAME*
+rm -f *$PACKAGE*.rpm
 
 exit 0
+}
+
+gen_ebuild()
+{
+        # FIXME: how to get build package name?
+        #PKGNAME=`querypackage "$SRPMNAME" NAME`
+        # FIXME: problem with various version
+	LOCUSER=korinfer
+	WORKDIR=/home/$LOCUSER/tmp
+	RPMSDIR=/home/$LOCUSER/RPM/RPMS
+        BUILTRPM=$(ls -1 $RPMSDIR/*.rpm | grep $PACKAGE | tail -n1)
+	test -r ${BUILTRPM} || return 1
+	mkdir -p $WORKDIR
+	cd $WORKDIR
+
+	EBUILDVERSION=`querypackage "$BUILTRPM" VERSION`
+	EBUILDRELEASE=`querypackage "$BUILTRPM" RELEASE`
+	EBUILDARCH=`querypackage "$BUILTRPM" ARCH`
+	#test: emerge doesn't work with release
+	#EBUILDFILE=${WORKDIR}/${PACKAGE}-${EBUILDVERSION}-${EBUILDRELEASE}.ebuild
+	EBUILDFILE=${WORKDIR}/${PACKAGE}-${EBUILDVERSION}.ebuild
+	export $EBUILDFILE
+
+	echo "# Copyright 1999-2007 Gentoo Foundation" > $EBUILDFILE
+	echo "# Distributed under the terms of the GNU General Public License v2" >> $EBUILDFILE
+	#echo '# $Header: $' >> $EBUILDFILE
+	echo >> $EBUILDFILE
+	echo "MY_R=$EBUILDRELEASE" >> $EBUILDFILE
+	echo "MY_ARCH=$EBUILDARCH" >> $EBUILDFILE
+        DESCRIPTION=`querypackage "$BUILTRPM" SUMMARY`
+	echo "DESCRIPTION=\"$DESCRIPTION\"" >> $EBUILDFILE
+	HOMEPAGE=`querypackage "$BUILTRPM" URL`
+	echo "HOMEPAGE=$HOMEPAGE" >> $EBUILDFILE
+
+	#FIXME: how to define SRC_URI
+	TARGET="tar.bz2"
+	SRC_URI="$DESTURL/\${PN}-\${PV}-\${MY_R}.\${MY_ARCH}.${TARGET}"
+
+	echo "SRC_URI=$SRC_URI" >> $EBUILDFILE
+	LICENSE=`querypackage "$BUILTRPM" LICENSE`
+	echo "LICENSE=$LICENSE" >> $EBUILDFILE
+	echo 'SLOT="0"' >> $EBUILDFILE
+	echo 'KEYWORDS="-* x86 amd64"' >> $EBUILDFILE
+	echo >> $EBUILDFILE
+	echo "src_unpack() {" >> $EBUILDFILE
+	echo 'unpack ${A}' >> $EBUILDFILE
+	echo "}" >> $EBUILDFILE
+	echo >> $EBUILDFILE
+	echo "src_install() {" >> $EBUILDFILE
+	echo 'cp -pR "${WORKDIR}" "${D}"' >> $EBUILDFILE
+	echo "}" >> $EBUILDFILE
+
+	cp $EBUILDFILE $RPMSDIR
+
+# delete built RPM
+	rm -rf $BUILTRPM
+
+        #get file hierarchy
+#	cp $BUILTRPM ${WORKDIR}/${PACKAGE}
+#        rpm2cpio $BUILTRPM | cpio -dimv || fatal "error with rpm2cpio"
+#        rm -f $BUILTRPM
+
+        # create package with the PACKAGE name (not src.rpm name)
+#        rm -f ../$TARGETPKG
+#        ebuild $EBUILDFILE package || fatal
+#        cd -
+
+}
+
+install_gentoo()
+{
+        emerge ../$TARGETPKG
+}
+
+clean_gentoo()
+{
+        echo "Cleaning..."
+        rm -rf $WRKDIR
+}
+
+case $COMMAND in
+        "build")
+                build_gentoo
+                ;;
+        "generate")
+		gen_ebuild
+                ;;
+        "install")
+                install_gentoo
+                ;;
+        "clean")
+                clean_gentoo
+                ;;
+        *)
+                fatal "Unknown command $COMMAND"
+                ;;
+esac
