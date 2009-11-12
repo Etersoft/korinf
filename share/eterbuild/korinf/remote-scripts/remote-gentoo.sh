69651ed662c46bbf710e066bf9e17cf43b6a9691
Yuri Fil <yurifil@etersoft.ru>
2009-11-12 16:57:58 +0300

    fixes for ebuilds

--- share/eterbuild/korinf/remote-scripts/remote-gentoo.sh
+++ share/eterbuild/korinf/remote-scripts/remote-gentoo.sh
@@ -7,7 +7,6 @@
 # positional parameters
 COMMAND=$1
 PACKAGE="$2"
-#RPMSDIR="$3"
 SOURCEURL="$3"
 DESTURL="$5"
 
@@ -24,39 +23,8 @@ fatal()
         exit 1
 }
 
-
-build_gentoo()
-{
-        RPMBUILDNODEPS="--nodeps"
-        RPMBUILDROOT="/home/$INTUSER/RPM/BUILD/$PACKAGE-$PKGVERSION"
-        # FIXME: x86_64 support
-        BUILDARCH=i586
-        rpmbuild -v --rebuild $RPMBUILDNODEPS --buildroot $RPMBUILDROOT $SRPMNAME --target $BUILDARCH
-}
-
-#convert to tar.gz
-convert_gentoo()
-{
-LOCUSER=korinfer
-BUILDERHOME=/home/$LOCUSER
-#PORTAGEDIR=/usr/local/portage
-#TEMPDIR=$BUILDERHOME/tmp/$PACKAGE
-#RPMDIR=$BUILDERHOME/RPM/BP
-
-echo
-echo "Changing to RPMSDIR=$RPMSDIR"
-pushd $RPMSDIR
-echo "Converting rpm to tar.gz"
-rpm2targz *$BUILDNAME*
-rm -f *$PACKAGE*.rpm
-
-exit 0
-}
-
 gen_ebuild()
 {
-        # FIXME: how to get build package name?
-        #PKGNAME=`querypackage "$SRPMNAME" NAME`
         # FIXME: problem with various version
 	LOCUSER=korinfer
 	WORKDIR=/home/$LOCUSER/tmp
@@ -70,48 +38,42 @@ gen_ebuild()
 	EBUILDRELEASE=`querypackage "$BUILTRPM" RELEASE`
 	EBUILDARCH=`querypackage "$BUILTRPM" ARCH`
 	#test: emerge doesn't work with release
-	#EBUILDFILE=${WORKDIR}/${PACKAGE}-${EBUILDVERSION}-${EBUILDRELEASE}.ebuild
 	EBUILDFILE=${WORKDIR}/${PACKAGE}-${EBUILDVERSION}.ebuild
 	export $EBUILDFILE
 
-	echo "# Copyright 1999-2007 Gentoo Foundation" > $EBUILDFILE
-	echo "# Distributed under the terms of the GNU General Public License v2" >> $EBUILDFILE
-	#echo '# $Header: $' >> $EBUILDFILE
-	echo >> $EBUILDFILE
-	echo "MY_R=$EBUILDRELEASE" >> $EBUILDFILE
-	echo "MY_ARCH=$EBUILDARCH" >> $EBUILDFILE
         DESCRIPTION=`querypackage "$BUILTRPM" SUMMARY`
-	echo "DESCRIPTION=\"$DESCRIPTION\"" >> $EBUILDFILE
 	HOMEPAGE=`querypackage "$BUILTRPM" URL`
-	echo "HOMEPAGE=$HOMEPAGE" >> $EBUILDFILE
-
+	LICENSE=`querypackage "$BUILTRPM" LICENSE`
 	#FIXME: how to define SRC_URI
 	TARGET="tar.bz2"
 	SRC_URI="$DESTURL/\${PN}-\${PV}-\${MY_R}.\${MY_ARCH}.${TARGET}"
+	
+	cat > $EBUILDFILE << EOF
+# Copyright 1999-2007 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
 
-	echo "SRC_URI=$SRC_URI" >> $EBUILDFILE
-	LICENSE=`querypackage "$BUILTRPM" LICENSE`
-	echo "LICENSE=$LICENSE" >> $EBUILDFILE
-	echo 'SLOT="0"' >> $EBUILDFILE
-	echo 'KEYWORDS="-* x86 amd64"' >> $EBUILDFILE
-	echo >> $EBUILDFILE
-	echo "src_unpack() {" >> $EBUILDFILE
-	echo 'unpack ${A}' >> $EBUILDFILE
-	echo "}" >> $EBUILDFILE
-	echo >> $EBUILDFILE
-	echo "src_install() {" >> $EBUILDFILE
-	echo 'cp -pR "${WORKDIR}" "${D}"' >> $EBUILDFILE
-	echo "}" >> $EBUILDFILE
+MY_R=$EBUILDRELEASE
+MY_ARCH=$EBUILDARCH
+DESCRIPTION=$DESCRIPTION
+HOMEPAGE=$HOMEPAGE
+SRC_URI=$SRC_URI
+LICENSE=$LICENSE
+SLOT="0"
+KEYWORDS="-* x86 amd64"
+
+src_unpack() {
+unpack \${A}
+}
+
+src_install() {
+cp -pR * "\${D}"
+}
+EOF
 
 	cp $EBUILDFILE $RPMSDIR
 
 # delete built RPM
 	rm -rf $BUILTRPM
-
-        #get file hierarchy
-#	cp $BUILTRPM ${WORKDIR}/${PACKAGE}
-#        rpm2cpio $BUILTRPM | cpio -dimv || fatal "error with rpm2cpio"
-#        rm -f $BUILTRPM
 
         # create package with the PACKAGE name (not src.rpm name)
 #        rm -f ../$TARGETPKG
