dc8ecc38ea6c2b6d2f0b450963671eba432cf56a
Vitaly Lipatov <lav@etersoft.ru>
2009-11-17 21:36:15 +0300

    common: add filter_distro_list

--- tests/test_distro_list.sh
+++ tests/test_distro_list.sh
@@ -10,12 +10,12 @@ check()
 	local REPL
 	TEST1="$1"
 	REPL1="$2"
-	REAL=`get_distro_list "$TEST1"`
-	echo
+	REAL=`get_distro_list "$TEST1" | filter_distro_list`
 	echo -n "Source line: '$TEST1' with result '$REAL' (wait for '$REPL1') "
 	#REPL=`echo $TEST1 | sed -r -e $NRL`
 	#REPL=`echo $TEST1 | perl -pi "$NRL"`
 	[ "$REPL1" != "$REAL" ] && failure || success
+	echo
 	echo
 }
 
@@ -25,9 +25,11 @@ check "SomeDistro/2010 SomeLinux/2020" "SomeDistro/2010 SomeLinux/2020"
 cat <<EOF >test1
 #
 SomeDistro/2010
+SomeDistro/2011
 SomeLinux/2020
 EOF
 check test1 "SomeDistro/2010
+SomeDistro/2011
 SomeLinux/2020"
 
 cat <<EOF >test2
@@ -37,6 +39,24 @@ EndLinux/2038
 EOF
 check test2 "EndLinux/2038
 SomeDistro/2010
+SomeDistro/2011
+SomeLinux/2020"
+
+SKIPBUILDLIST="EndLinux"
+echo "with skip $SKIPBUILDLIST"
+check test2 "SomeDistro/2010
+SomeDistro/2011
+SomeLinux/2020"
+
+SKIPBUILDLIST="SomeDistro/2010"
+echo "with skip $SKIPBUILDLIST"
+check test2 "EndLinux/2038
+SomeDistro/2011
+SomeLinux/2020"
+
+SKIPBUILDLIST="SomeDistro"
+echo "with skip $SKIPBUILDLIST"
+check test2 "EndLinux/2038
 SomeLinux/2020"
 
 rm -f test1 test2
