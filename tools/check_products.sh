56a886f7c00057b8dd611b744b4238e70ada6b38
Yuri Fil <yurifil@etersoft.ru>
2009-11-28 18:02:57 +0300

    update

--- tools/check_products.sh
+++ tools/check_products.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #script that checks built packages for our products
-#usage: ./check_products.sh SYSLIST
+#usage: ./check_products.sh SYSLIST CHECKPROJECT
 
 # load common functions, compatible with local and installed script
 . `dirname $0`/../share/eterbuild/korinf/common
@@ -10,15 +10,16 @@
 define_paths()
 {
 # выбираем в KORINFROOTDIR каталоги, содержащие сборочные скрипты
-	find $1 -name bin-\* -print
+	find $1 -name bin-\* -print | grep -v old
 }
 
 define_components()
 {
 # выбираем файлы .sh в $PATHTOSCRIPT, исключая файлы foo-all.sh, release-check, cabextract
 # FIXME: убрать проверку dkms-* для не-Мандрив: например, через переменную ADDITIONALGREPS, заполняемую по необходимости
-	DIRCONTENTS=`find $1 -maxdepth 1 -print | grep ".sh" | grep -v all | grep -v release-check | grep -v cabextract`
-	#| grep -v dkms-aksparlnx`
+	DIRCONTENTS=`find $1 -maxdepth 1 -print | grep ".sh" | grep -v all | grep -v release-check`
+	#| grep -v cabextract
+	#| grep -v dkms-aksparlnx
 	#$ADDITIONALGREPS
 	for i in $DIRCONTENTS ; do
 		basename $i .sh
@@ -30,14 +31,22 @@ grep_script()
 	#FIXME: Highlight this with another color
 	echo "Checking $1 for $2"
 # запускаем сборочные скрипты с параметром -c, выводим строки, содержащие сообщения об устаревших или пропущенных сборках
-	$PATHTOSCRIPT/$1.sh -c $2 | grep -e OBS -e MISSED | grep -v Legend | grep -v link | grep -v error
+	$PATHTOSCRIPT/$1.sh -c $2 | grep -e OBS -e MISSED | grep -v Legend | grep -v link | grep -v error || echo "Everything is built"
 }
 
 
 #start script
 KORINFROOTDIR="../"
-PRODUCTPATHS=`define_paths $KORINFROOTDIR`
 CHECKSYSLIST=$1
+CHECKPROJECT=$2
+
+if [ -z $CHECKPROJECT ] ; then
+    PRODUCTPATHS=`define_paths $KORINFROOTDIR`
+else
+    PRODUCTPATHS=$KORINFROOTDIR/bin-${CHECKPROJECT}
+fi
+
+echo PRODUCTPATHS=$PRODUCTPATHS
 
 for PATHTOSCRIPT in $PRODUCTPATHS ; do
 	COMPONENTS=`define_components $PATHTOSCRIPT`
