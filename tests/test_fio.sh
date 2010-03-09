9fe18683f6dd243bbaa9a52261224cb9ca8d324c
Vitaly Lipatov <lav@etersoft.ru>
2010-03-09 15:18:04 +0300

    add messages/fio module, add get_dear_from_fio func and update fio test

--- tests/test_fio.sh
+++ tests/test_fio.sh
@@ -1,22 +1,9 @@
 #!/bin/sh
 
-get_name_from_fio()
-{
-# cut failed with names if field 2 is missed
-#	echo "$@" | cut -d" " -f2
-	echo $1 | awk '{print $2}'
-}
+# load common functions, compatible with local and installed script
+. `dirname $0`/../share/eterbuild/korinf/common
 
-# ФИО -> И Ф
-get_fio_for_email()
-{
-	local FNAME="$(get_name_from_fio "$@")"
-	local LNAME="$(echo "$@" | cut -d" " -f1)"
-	local SNAME="$(echo "$@" | cut -d" " -f3)"
-	[ -z "$FNAME" ] || echo -n "$FNAME "
-	echo "$LNAME"
-}
-
+kormod messages/fio
 
 check()
 {
@@ -28,7 +15,21 @@ check_fio()
 	check "$2" "$1" "$(get_fio_for_email "$2")"
 }
 
+
 check_fio "Пётр Иванов" "Иванов Пётр Петрович"
 check_fio "Пётр Иванов" "Иванов  Пётр  Петрович"
 check_fio "Пётр Иванов" "Иванов Пётр"
 check_fio "Иванов" "Иванов"
+
+
+check_dear()
+{
+	check "$2" "$1" "$(get_dear_from_fio "$2" "$3")"
+}
+
+check_dear "Пётр Петрович" "Иванов Пётр Петрович" "покупатель"
+check_dear "Пётр Петрович" "Иванов  Пётр  Петрович" "покупатель"
+check_dear "Пётр" "Иванов Пётр" "покупатель"
+check_dear "Иванов" "Иванов" "покупатель"
+check_dear "покупатель" "" "покупатель"
+
