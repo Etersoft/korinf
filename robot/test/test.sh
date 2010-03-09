7efe170925328858bf9d2d77348d462e23336c28
Vitaly Lipatov <lav@etersoft.ru>
2010-03-09 22:04:35 +0300

    wine-etersoft: rewrite download, use http by default, fix -gl situation for general and for CAD, move HASP download from separate dir , rewrite many texts.

--- robot/test/test.sh
+++ robot/test/test.sh
@@ -4,7 +4,9 @@ AROBOTDIR=$(pwd)/..
 
 #cp tasks/test-postgres.task ./try.task
 #cp tasks/test-rx.task ./try.task
-cp tasks/test-wine-etersoft.task ./try.task
+#cp tasks/test-wine-etersoft.task ./try.task
+cp tasks/test-wine-etersoft-network.task ./try.task
+#cp tasks/test-wine-etersoft-cad.task ./try.task
 #cp tasks/test-wine.task ./try.task
 #cp tasks/test-selta.task ./try.task
 $AROBOTDIR/arobot.sh try.task
