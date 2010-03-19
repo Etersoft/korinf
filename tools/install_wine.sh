ba3a5506543aaf71205b1bbcc5b52f36478b015e
Vitaly Lipatov <lav@etersoft.ru>
2010-03-19 13:02:40 +0300

    fix install on Ubuntu

--- tools/install_wine.sh
+++ tools/install_wine.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Script to install WINE@Etersoft to Sisyphus based system
 
@@ -22,25 +22,56 @@ PROJECTVERSION=$1
 
 get_version_release()
 {
-SOURCEPATH=$TARGETPATH/../../../sources
+	SOURCEPATH=$TARGETPATH/../../../../sources
+	[ -d "$SOURCEPATH" ] || SOURCEPATH=$TARGETPATH/../../../sources
 
 if [ -z "$VERSION" ] ; then
 	BUILDSRPM="$SOURCEPATH/$(ls -1 $SOURCEPATH/$BUILDNAME-[0-9]*.src.rpm | last_rpm).src.rpm"
-	# почему-то не последний пакет
+	# FIXME: почему-то не последний пакет
 	echo $BUILDSRPM
 	VERSION=`rpm -qp --queryformat "%{VERSION}" $BUILDSRPM`
 	RELEASE=`rpm -qp --queryformat "%{RELEASE}" $BUILDSRPM`
+	if [ "$DISTRIB_ID" = "Ubuntu" ] ; then
+		RELEASE=eter`echo "$RELEASE" | sed -e "s|\([a-zA-Z]*\)\([0-9\.]\)[^0-9\.]*|\2|" `ubuntu
+	fi
 fi
 }
 
 pkg_is_installed()
 {
 	test -n "$INITIAL" && return
-	rpm -q "$1" &>/dev/null
+	if [ "$DISTRIB_ID" = "Ubuntu" ] ; then
+		dpkg -s "$1" &>/dev/null
+	else
+		rpm -q "$1" &>/dev/null
+	fi
+}
+
+install_pkg()
+{
+	echo $LIST
+	if [ "$DISTRIB_ID" = "Ubuntu" ] ; then
+		sudo dpkg -i $LIST $FORCE
+	else
+		rpmU $LIST $FORCE
+	fi
 }
 
 #############
-TARGETPATH=$WINEPUB_PATH/$PROJECTVERSION/WINE/ALTLinux/Sisyphus
+SYSTEM=
+if [ -r "/etc/lsb-release" ] ; then
+	. /etc/lsb-release
+	SYSTEM=$DISTRIB_ID/$DISTRIB_RELEASE
+	if [ $DEFAULTARCH = "x86_64" ] ; then
+		SYSTEM="x86_64/$SYSTEM"
+	fi
+	EXT=deb
+else
+	SYSTEM=ALTLinux/Sisyphus
+	EXT=rpm
+fi
+
+TARGETPATH=$WINEPUB_PATH/$PROJECTVERSION/WINE/$SYSTEM
 get_version_release
 
 if cd $TARGETPATH ; then
@@ -48,21 +79,20 @@ if cd $TARGETPATH ; then
 
 	LIST=
 	for i in $BUILDNAME ; do
-		pkg_is_installed $i && LIST="$LIST $i-$VERSION-*$RELEASE.*.rpm"
+		pkg_is_installed $i && LIST="$LIST $i[-_]$VERSION-*$RELEASE[._]*.$EXT"
 	done
 
 	for i in lib$BUILDNAME-devel $BUILDNAME-gl $BUILDNAME-twain ; do
-		pkg_is_installed $i && LIST="$LIST extra/$i-$VERSION-*$RELEASE.*.rpm"
+		pkg_is_installed $i && LIST="$LIST extra/$i[-_]$VERSION-*$RELEASE[._]*.$EXT"
 	done
 
-	echo $LIST
-	rpmU $LIST $FORCE
+	install_pkg
 fi
 
 #############
 VERSION=
 BUILDNAME=$BUILDNAME-sql
-TARGETPATH=$WINEETER_PATH/$PROJECTVERSION/WINE-SQL/ALTLinux/Sisyphus
+TARGETPATH=$WINEETER_PATH/$PROJECTVERSION/WINE-SQL/$SYSTEM
 get_version_release
 
 cd $TARGETPATH || fatal "Can't cd"
@@ -70,9 +100,7 @@ pwd
 
 LIST=
 for i in $BUILDNAME ; do
-	pkg_is_installed $i && LIST="$LIST $i-$VERSION-*$RELEASE.*.rpm"
+	pkg_is_installed $i && LIST="$LIST $i[-_]$VERSION-*$RELEASE[._]*.$EXT"
 done
 
-echo $LIST
-rpmU $LIST $FORCE
-
+install_pkg
