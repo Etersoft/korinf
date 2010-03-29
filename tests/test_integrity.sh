bd5a6705420b3b4698bc1537ae7b209b93e8538b
Vitaly Lipatov <lav@etersoft.ru>
2010-03-29 13:33:11 +0400

    add check_integrity module with package integrity checking

---
+++ tests/test_integrity.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 2006 (c) Etersoft www.etersoft.ru
+# Public domain
+
+# load common functions, compatible with local and installed script
+. `dirname $0`/../share/eterbuild/korinf/common
+
+kormod check_integrity
+
+check_files_integrity /var/ftp/pub/Etersoft/WINE@Etersoft/1.0.12-eter4/sources
+check_files_integrity /var/ftp/pub/Etersoft/WINE@Etersoft/1.0.12-eter4/sources/tarball
+check_files_integrity /var/ftp/pub/Etersoft/WINE@Etersoft/1.0.12-eter4/WINE/Debian/5.0
