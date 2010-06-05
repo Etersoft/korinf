baf34ed71542ce2c49bc8c8d9018d7aad6f141f9
Vitaly Lipatov <lav@etersoft.ru>
2010-06-05 05:01:20 +0400

    rewrite build-testing and build-unstable scripts

---
+++ tools/cron/build-funcs.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+fatal()
+{
+	echo "Error $@"
+	exit 1
+}
+
+
+jump_to_repo()
+{
+	cd $TEMPREPODIR/ || fatal
+	
+	if ! test -d $WORKBRANCH ; then
+		git clone $REPO $WORKBRANCH || fatal "can't clone $REPO"
+		cd $WORKBRANCH
+		git checkout -b $WORKBRANCH $REPOALIAS/$WORKBRANCH || fatal "can't checkout"
+		exit
+	fi
+	cd $WORKBRANCH
+}
+
+pull_and_log()
+{
+	# check repo
+	git rev-parse HEAD || fatal "check repo failed"
+
+	# save TAG for last commit
+	TAG=$(git rev-parse HEAD)
+
+	# try pull and exit if all up-to-date
+	gpull -c $REPOALIAS $WORKBRANCH && { echo "No work" ; exit ; }
+
+	NEWTAG=$(git rev-parse HEAD)
+
+	[ "$TAG" = "$NEWTAG" ] && fatal "last commit is not changed after update"
+
+	# сформировать лог, обновить spec с пред. момента до обновления
+	rpmlog -s -l $TAG
+}
+
+pub_and_push()
+{
+	# публикуем на сборку
+	rpmpub -r $WORKTARGET
+
+	# will works only if REPOALIAS is origin :)
+	gpush $REPOALIAS $WORKBRANCH || fatal
+}
+
--- tools/cron/build-unstable.sh
+++ tools/cron/build-unstable.sh
@@ -1,31 +1,22 @@
 #!/bin/sh
 
-fatal()
-{
-	echo "Error $@"
-	exit 1
-}
+# Обновляет репозиторий и выкладывает новую сборку в случае необходимости
 
-TEMPREPODIR=/srv/$USER/Projects
+. $(dirname $0)/build-funcs.sh
 
 export LC_ALL=C
 
-cd $TEMPREPODIR/ || fatal
-
+TEMPREPODIR=/srv/$USER/Projects
 REPO=git.office:/projects/eterhack.git
-if ! test -d eterhack ; then
-	git clone $REPO || fatal "can't clone $REPO"
-	git checkout -b eterhack origin/eterhack || fatal "can't checkout eterhack"
-	cd eterhack
-else
-	cd eterhack
-	git pull origin eterhack || fatal "can't pull"
-fi
+REPOALIAS=origin
+WORKBRANCH=eterhack
+WORKTARGET=unstable
 
-cd etersoft || fatal "can't cd"
-
-rpmpub -r unstable wine-etersoft.spec
+jump_to_repo
+pull_and_log
+pub_and_push
 
 cd /srv/$USER/Projects/git/korinf/bin-wine || cd $HOME/Projects/korinf/bin-wine || fatal "can't CD"
 
-./wine-etersoft.sh test unstable
+./wine-etersoft.sh test $WORKTARGET
+
