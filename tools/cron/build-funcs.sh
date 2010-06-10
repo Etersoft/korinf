5a5ef72237d71f065d713f7ce13d236a9e07ef0f
Vitaly Lipatov <lav@etersoft.ru>
2010-06-10 19:03:16 +0400

    fix version increment in the project

--- tools/cron/build-funcs.sh
+++ tools/cron/build-funcs.sh
@@ -20,10 +20,22 @@ jump_to_repo()
 	cd $WORKBRANCH
 }
 
+
+update_version()
+{
+	local SPECNAME=etersoft/wine-etersoft.spec
+	test -f VERSION && test -f $SPECNAME || fatal "incorrect current dir $(pwd) or repo"
+
+	echo "WINE@Etersoft version $(get_version $SPECNAME)-eter$(get_numrelease $SPECNAME)" >VERSION
+	# rebuild needed files
+	autoreconf -f || fatal "autoreconf failed"
+	git commit --amend VERSION configure include/config.h.in
+}
+
 pull_and_log()
 {
 	# check repo
-	git rev-parse HEAD || fatal "check repo failed"
+	git rev-parse HEAD >/dev/null || fatal "check repo failed"
 
 	# save TAG for last commit
 	TAG=$(git rev-parse HEAD)
@@ -37,6 +49,9 @@ pull_and_log()
 
 	# сформировать лог, обновить spec с пред. момента до обновления
 	rpmlog -s -l $TAG
+
+	# обновляем VERSION и объединяем с предыдущим коммитом
+	update_version
 }
 
 pub_and_push()
