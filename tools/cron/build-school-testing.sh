e5a58944eba1317efa3f2cc9ad8f69405ceb23d5
Vitaly Lipatov <lav@etersoft.ru>
2011-03-06 00:18:48 +0300

    cron/build: add school-testing script

---
+++ tools/cron/build-school-testing.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Обновляет репозиторий и выкладывает новую сборку в случае необходимости
+
+. $(dirname $0)/build-funcs.sh
+
+TEMPREPODIR=/srv/$USER/Projects
+REPO=git.office:/projects/eterhack.git
+REPOALIAS=origin
+WORKBRANCH=eter-1.7.0
+WORKTARGET=school-testing
+
+jump_to_repo
+pull_and_log
+pub_and_push
+
+korinf_wine test $WORKTARGET
+
