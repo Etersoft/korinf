b697bf644ab7eeef7e6caba86c195b32f837f9b5
Vitaly Lipatov <lav@etersoft.ru>
2011-04-04 21:00:15 +0400

    tools: update list_found_libs script

---
+++ tools/list_all_nfound.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+do_test()
+{
+
+SC=./list_nfound_libs.sh
+
+$SC stable
+$SC testing
+$SC 2.0.0
+$SC school
+$SC school-testing
+$SC unstable
+
+$SC last wine /var/ftp/pub/Etersoft/Wine-public
+$SC last wine-vanilla /var/ftp/pub/Etersoft/Wine-vanilla
+
+}
+
+TFILE=~/tmp/nfound_test_file.out
+touch "$TFILE"
+EMAILS="lav@etersoft.ru alexalv@etersoft.ru baraka@etersoft.ru"
+do_test >$TFILE.new 2>&1
+DIFF=$(diff -u "$TFILE" "$TFILE.new")
+if [ -n "$DIFF" ] ; then
+	echo "$DIFF" | mutt $EMAILS -s "Changes in wine's not found libs"
+fi
+mv -f $TFILE.new $TFILE
--- tools/list_nfound_libs.sh
+++ tools/list_nfound_libs.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
 # Выводит сообщения о ненайденных вайновским configure библиотеках
 #
+# ./script 1.2.3 wine /var/ftp/pub/Etersoft/Wine-public
 
 # load common functions, compatible with local and installed script
 . `dirname $0`/../share/eterbuild/korinf/common
 
-[ -n "$1" ] && VERSION=$1 || VERSION=last
+[ "$1" = "-v" ] && { VERBOSE=1 ; shift ; }
+IPATH="WINE"
+[ -n "$1" ] && VERSION="$1" || VERSION=last
+[ -n "$2" ] && PACKAGENAME="$2" || PACKAGENAME=wine-etersoft
+[ -n "$3" ] && { WINEPUB_PATH="$3" ; IPATH="" ; }
+[ -n "$4" ] && SKIPPED="$4" || SKIPPED="libcapi|OpenCL"
 NAME=$WINEPUB_PATH/$VERSION
-PACKAGENAME=wine-etersoft
 
+[ -n "$VERBOSE" ] && echo "Check in $NAME for $PACKAGENAME package..."
 for n in $(get_distro_list $NAME) ; do
-	echo
-	FILE=$NAME/WINE/$n/log/$PACKAGENAME.log.bz2
-	echo "$n: (`stat -c "%y" $FILE`)"
-	bzcat $FILE | grep "configure:" | grep development | grep found | sed -e "s|^configure:|          |g"
+	FILE=$NAME/$IPATH/$n/log/$PACKAGENAME.log.bz2
+	res=$(bzcat $FILE | grep "configure:" | grep development | grep found | sed -e "s|^configure:|          |g" | grep -E -v "($SKIPPED)")
+	if [ -n "$VERBOSE" ] || [ -n "$res" ] ; then
+		echo
+		echo "$NAME/$n: (`stat -c "%y" $FILE`)"
+		echo "$res"
+	fi
 done
