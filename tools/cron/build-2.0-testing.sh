e652f990025abc42d7cf82ae7e342734ebf8537e
Vitaly Lipatov <lav@etersoft.ru>
2012-04-11 21:50:39 +0400

    add build-2.0-testing

---
+++ tools/cron/build-2.0-testing.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Обновляет репозиторий и выкладывает новую сборку в случае необходимости
+
+. $(dirname $0)/build-funcs.sh
+
+TEMPREPODIR=/srv/$USER/Projects
+REPO=git.office:/projects/wine/eterhack.git
+REPOALIAS=origin
+WORKBRANCH=eter-2.0.0
+WORKTARGET=2.0-testing
+
+jump_to_repo
+pull_and_log
+pub_and_push
+
+korinf_wine test $WORKTARGET
+
+# run only on server
+#cd $(dirname $0)/.. || exit
+#./publish_wine_to_our_distro.sh 2.0
