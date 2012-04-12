deb5640e2dcb09ed784ede6fc676b2e407b4bc07
Vitaly Lipatov <lav@etersoft.ru>
2012-04-12 18:25:28 +0400

    fix wine priv autobuild

--- tools/cron/build-priv-2.0-testing.sh
+++ tools/cron/build-priv-2.0-testing.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# disable autorun from cron
+#[ -z "$1" ] && exit
+
 # Обновляет репозиторий и выкладывает новую сборку в случае необходимости
 
 . $(dirname $0)/build-funcs.sh
@@ -9,16 +12,17 @@ REPO=git.office:/projects/wine/wine-etersoft.git
 REPOALIAS=origin
 WORKBRANCH=eter-2.0.0
 WORKTARGET=2.0-testing
+SUBSPEC="local sql network"
 
 jump_to_repo
 
 pull_changes || exit
 
-for sp in local sql network ; do
+for sp in $SUBSPEC ; do
 	SPECNAME=wine-etersoft-$sp.spec
 	test -f VERSION && test -f $SPECNAME || fatal "incorrect current dir $(pwd) or repo"
 	# CURTAG and NEWTAG defined in pull_changes
-	rpmlog -r -l $SPECNAME $CURTAG $NEWTAG
+	rpmlog -r -l $SPECNAME $CURTAG $NEWTAG || fatal "can't rpmlog"
 	# uncommit
 	git reset --soft HEAD^
 	# обновляем VERSION и объединяем с предыдущим коммитом
@@ -31,9 +35,19 @@ echo "WINE@Etersoft version $(get_version $SPECNAME)-eter$(get_numrelease $SPECN
 # rebuild needed files
 autoreconf -f || fatal "autoreconf failed"
 
-git commit -a --amend -m "auto build with $(basename $0) script"
+git commit -a -m "auto build with $(basename $0) script"
 
-pub_and_push
+
+#pub_and_push
+for sp in $SUBSPEC ; do
+	SPECNAME=wine-etersoft-$sp.spec
+	# публикуем на сборку
+	rpmpub -r $WORKTARGET $SPECNAME
+done
+
+# push , but reset is failed
+# will works only if REPOALIAS is origin :)
+gpush -t $REPOALIAS $WORKBRANCH || git reset --hard $CURTAG
 
 # FIXME: some detect korinf placing
 cd /srv/$USER/Projects/git/korinf/bin-wine 2>/dev/null || cd $HOME/Projects/korinf/bin-wine || fatal "can't CD"
