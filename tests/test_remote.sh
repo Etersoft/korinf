53cf0e37cecca2acc122f940bac0557a4041a36d
Vitaly Lipatov <lav@etersoft.ru>
2012-12-08 13:31:50 +0400

    improve run_in_chroot, add initial remote build

---
+++ tests/test_remote.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# 2006 (c) Etersoft www.etersoft.ru
+# Public domain
+
+# load common functions, compatible with local and installed script
+. `dirname $0`/../share/eterbuild/korinf/common
+
+get_remote_host FreeBSD/8.2
+get_remote_commands FreeBSD/8.2
