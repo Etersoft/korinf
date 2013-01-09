e6ab15008a0ccb34252724e86f3490561283eb4c
Vitaly Lipatov <lav@etersoft.ru>
2013-01-09 13:07:31 +0400

    update tests

--- robot/test/test.sh
+++ robot/test/test.sh
@@ -5,7 +5,8 @@ AROBOTDIR=$(pwd)/..
 #cp tasks/test-postgres.task ./try.task
 #cp tasks/test-rx.task ./try.task
 #cp tasks/test-wine-etersoft.task ./try.task
-cp tasks/test-wine-etersoft-network.task ./try.task
+cp tasks/test-wine-etersoft-license-only.task ./try.task
+#cp tasks/test-selta-license-only.task ./try.task
 #cp tasks/test-wine-etersoft-cad.task ./try.task
 #cp tasks/test-wine.task ./try.task
 #cp tasks/test-selta.task ./try.task
