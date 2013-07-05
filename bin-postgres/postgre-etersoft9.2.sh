5f7cd7f900208912435e8052c51957838d79d5a0
Vitaly Lipatov <lav@etersoft.ru>
2013-07-05 21:37:45 +0400

    add pg 9.2 script

---
+++ bin-postgres/postgre-etersoft9.2.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# 2005, 2006, 2007, 2008 (c) Etersoft http://etersoft.ru
+# Author: Vitaly Lipatov <lav@etersoft.ru>
+# GNU Public License version 3
+
+#
+# load common functions, compatible with local and installed script
+. `dirname $0`/../share/eterbuild/korinf/common
+kormod korinf
+
+major=9.2
+pname=postgre-etersoft
+pnamever=${pname}${major}
+POSTGRESDIR=/var/ftp/pub/Etersoft/Postgres@Etersoft/
+
+# Проблема с тем, что пакет с библиотеками не -libs, а не libpq5.2
+export MAINFILES="${pnamever}-seltaaddon[-_][0-9] ${pnamever}[-_][0-9] ${pnamever}-contrib[-_][0-9] ${pnamever}-server[-_][0-9] libpq5.[0-9]-${major}eter"
+#export EXTRAFILES="${pnamever}-devel[-_][0-9] ${pnamever}-docs[-_][0-9] ${pnamever}-plperl[-_][0-9] ${pnamever}-plpython[-_][0-9] ${pnamever}-test[-_][0-9]"
+export EXTRAFILES="${pnamever}-docs[-_][0-9] ${pnamever}-plperl[-_][0-9] ${pnamever}-plpython[-_][0-9]"
+
+build_project $POSTGRESDIR ${pnamever} "" $@
