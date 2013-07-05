04d19c1817468096271cab0417c2cbc46ef01762
Vitaly Lipatov <lav@etersoft.ru>
2013-07-05 12:18:37 +0400

    fix path for watchbuilder

--- robot/service/queuewatcher.sh
+++ robot/service/queuewatcher.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-su - builder -c "/home/builder/Projects/korinf/robot/queuewatcher.sh $1 >/var/log/queuebuild.log 2>&1 &"
+su - builder -c "/srv/builder/Projects/korinf/robot/queuewatcher.sh $1 >/var/log/queuebuild.log 2>&1 &"
 sleep 2
