0925f452704832d8bcb44d384e9911157660f762
Vitaly Lipatov <lav@etersoft.ru>
2016-06-25 01:09:57 +0300

    improve build-strap for eepm and rpm-build-altlinux-compat

--- bin-common/build-strap.sh
+++ bin-common/build-strap.sh
@@ -1,1 +1,13 @@
-rpm-build-altlinux-compat.sh
+#!/bin/sh -x
+# 2005, 2006, 2007, 2008, 2009 (c) Etersoft http://etersoft.ru
+# Author: Vitaly Lipatov <lav@etersoft.ru>
+# GNU Public License version 3
+
+# install packages after build
+export BOOTSTRAP=1
+
+FIRST="$1"
+shift
+
+$(dirname $0)/../bin/korinf rpm-build-altlinux-compat "$FIRST" /var/ftp/pub/Etersoft/Sisyphus "$@"
+$(dirname $0)/../bin/korinf eepm "$FIRST" /var/ftp/pub/Etersoft/Sisyphus "$@"
