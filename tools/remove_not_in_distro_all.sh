c7c75d84140ae9c24b6e2b3616796eeda76a4f39
Vitaly Lipatov <lav@etersoft.ru>
2016-07-21 16:42:06 +0300

    improve tools/remove_not_in_distro

--- tools/remove_not_in_distro_all.sh
+++ tools/remove_not_in_distro_all.sh
@@ -2,7 +2,8 @@
 #find /var/ftp/pub/Etersoft/WINE@Etersoft -mindepth 1 -maxdepth 1 -type d | xargs -n1 ./remove_not_in_distro.sh WINE
 #find /var/ftp/pub/Etersoft/WINE@Etersoft -mindepth 1 -maxdepth 1 -type d | xargs -n1 ./remove_not_in_distro.sh fonts
 #find /var/ftp/pub/Etersoft/CIFS@Etersoft -mindepth 1 -maxdepth 1 -type d | xargs -n1 ./remove_not_in_distro.sh
+find /var/ftp/pub/Etersoft/RX@Etersoft -mindepth 1 -maxdepth 1 -type d | xargs -n1 ./remove_not_in_distro.sh
 
-for i in Local Network SQL CAD School ; do
-	find /var/ftp/pvt/Etersoft/WINE@Etersoft -mindepth 1 -maxdepth 1 -type d | xargs -n1 ./remove_not_in_distro.sh WINE-$i
-done
+#for i in Local Network SQL CAD School ; do
+#	find /var/ftp/pvt/Etersoft/WINE@Etersoft -mindepth 1 -maxdepth 1 -type d | xargs -n1 ./remove_not_in_distro.sh WINE-$i
+#done
