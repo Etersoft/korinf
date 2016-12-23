be8a4c9a5d6bbb963d6d6d6045052ad20cac3e57
Vitaly Lipatov <lav@etersoft.ru>
2016-12-24 02:05:49 +0300

    add bin-rx scripts

---
+++ bin-rx/rxclient.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# 2005, 2006, 2007, 2008 (c) Etersoft http://etersoft.ru
+# Author: Vitaly Lipatov <lav@etersoft.ru>
+# GNU Public License version 3
+
+#
+# load common functions, compatible with local and installed script
+. `dirname $0`/../share/eterbuild/korinf/common
+kormod korinf
+
+build_project /var/ftp/pvt/Etersoft/RX@Etersoft rxclient "" $@
