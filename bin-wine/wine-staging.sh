0b72d235de8ea12cc5459f2ea2f08bcf367a8eb3
Vitaly Lipatov <lav@etersoft.ru>
2017-07-14 20:26:47 +0300

    add wine-staging.sh script

---
+++ bin-wine/wine-staging.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# 2005, 2006, 2007, 2008 (c) Etersoft http://etersoft.ru
+# Author: Vitaly Lipatov <lav@etersoft.ru>
+# GNU Public License version 3
+#
+
+# load common functions, compatible with local and installed script
+. `dirname $0`/../share/eterbuild/korinf/common
+kormod korinf
+
+NAME=wine
+
+MAINFILES="$NAME[-_][0-9] lib$NAME[-_][0-9] lib$NAME-gl lib$NAME-twain lib$NAME-devel"
+
+build_project $WINEPUB_PATH/../Wine-staging $NAME "" $@
